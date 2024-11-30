4556afb08bb5cffca3a33d545c0253d8602c5a2f
Роберт <dificalabc@gmail.com>
2024-11-30 19:49:04 +0500

    ues

--- draft.py
+++ draft.py
@@ -1,5 +1,3 @@
-first = ['Strings', 'Student', 'Computers']
-second = ['Строка', 'Урбан', 'Компьютер']
-a = range(len(first), len(second) + 1)
-print(list(a))
-print(len(first))
+import random
+
+print(random.randint(1, 10))
